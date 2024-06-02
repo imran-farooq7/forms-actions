58cce86b992d8d8eb213227c96c012ec3aed4aa6
Imran Farooq <imran_farooq7@hotmail.com>
2024-06-03 02:09:44 +0500

    layout updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
 import "./global.css";
 
 export const metadata = {
-  title: "Next.js Forms Example",
-  description: "Example application with forms and Postgres.",
+	title: "Next.js Form Example",
+	description: "Example application with forms and Postgres.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
